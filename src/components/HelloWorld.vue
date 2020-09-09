aa19d3671cb4577a1736c219557136ea202c6504
wuyp <wuyp@fsmeeting.com>
2020-09-09 17:02:50 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,104 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="content">
+    <div class="wrapper">
+      <div>{{ num }}</div>
+      <ul class="row_ul">
+        <li v-for="(item, index) in items" :key="index">
+          <ul class="item_ul">
+            <li
+              v-for="(row, index) in item"
+              :key="index"
+              @click="operateNum(row)"
+            >
+              {{ row.content }}
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      num: "0",
+      lastNum: "0",
+      items: {
+        row1: [
+          { content: "AC" },
+          { content: "DEL" },
+          { content: "%" },
+          { content: "*" },
+        ],
+        row2: [
+          { content: 7 },
+          { content: 8 },
+          { content: 9 },
+          { content: "*" },
+        ],
+        row3: [
+          { content: 4 },
+          { content: 5 },
+          { content: 6 },
+          { content: "-" },
+        ],
+        row4: [
+          { content: 1 },
+          { content: 2 },
+          { content: 3 },
+          { content: "+" },
+        ],
+        row5: [{ content: 0 }, { content: "." }, { content: "=" }],
+      },
+    };
+  },
+  methods: {
+    operateNum(row) {
+      var type = row.content;
+      console.log(type);
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.content {
+  height: 100%;
+  position: relative;
+  font-size: 0.52rem;
+}
+.content .wrapper {
+  padding: 0 0.3rem;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+.content .wrapper > div {
+  text-align: right;
+  padding: 0.5rem 0.3rem;
+  font-size: 0.8rem;
 }
 ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.item_ul {
+  display: flex;
+  justify-content: space-between;
 }
-a {
-  color: #42b983;
+.item_ul li {
+  border-radius: 50%;
+  border: 1px solid #ffffff;
+  width: 1.4rem;
+  height: 1.4rem;
+  line-height: 1.3rem;
+  margin-bottom: 0.2rem;
+}
+.row_ul li:last-of-type li:first-of-type {
+  width: 3.3rem;
+  border-radius: 50rem;
 }
 </style>
